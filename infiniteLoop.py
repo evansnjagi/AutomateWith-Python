0f47635adfb2d5c1a6b4b0841e20b92672ea14e3
Evanskarago <148349906+evansnjagi@users.noreply.github.com>
2025-10-11 20:27:19 +0300

    Create infiniteLoop.py

---
+++ infiniteLoop.py
@@ -0,0 +1,10 @@
+# Write a program that prints hello world five times
+
+# instantiation
+spam = 0
+
+while spam < 5: # (while clause with the condition to be met)
+  # indented block of code 
+   print("Hello, world")
+   # increament after every check
+   spam += 1
